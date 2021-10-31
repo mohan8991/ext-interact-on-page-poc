af0d716c65bb4d2620ff1b02e1f6c8266348a5f3
mohan8991 <mohan.gopikrishna@servicenow.com>
2021-10-31 09:11:49 -0700

    adding on/off based on chrome extension

--- background.js
+++ background.js
@@ -1,5 +1,51 @@
 console.log("hola");
 
-chrome.tabs.onActivated.addListener(tab => {
-    chrome.tabs.executeScript(null, {runAt: 'document_start',file: './foreground.js'}, () => console.log("inject done"))
-})
+// chrome.tabs.onActivated.addListener(tab => {
+    
+// })
+
+
+var button = {
+    "init": function (callback) {
+    //   chrome.storage.local.get({"context": "page"}, function (storage) {
+    //     interface.url = interface.path + '?' + storage.context;
+    //     /*  */
+    //     app.button.icon(storage.context === "page" ? "OFF" : "ON", null);
+    //     chrome.browserAction.setPopup({"popup": (storage.context === "popup" ? interface.url : '')}, callback);
+    //   });
+    },
+    "click": function () {
+        chrome.tabs.executeScript(null, {runAt: 'document_start',file: './foreground.js'}, () => console.log("inject done"))
+    }
+  };
+
+  var contextmenu = {
+    "click": function (e) {
+      chrome.storage.local.get({"context": "page"}, function (storage) {
+        interface.close(storage.context);
+        /*  */
+        interface.url = interface.path + '?' + e.menuItemId;
+        chrome.storage.local.set({"context": e.menuItemId}, function () {
+          var popup = e.menuItemId === "popup" ? interface.url : '';
+          app.button.icon(e.menuItemId === "page" ? "OFF" : "ON", null);
+          chrome.browserAction.setPopup({"popup": popup}, function () {});
+        });
+      });
+    },
+    "create": {
+      "button": function () {
+        chrome.storage.local.get({"context": "page"}, function (storage) {
+          var popup = storage.context === "popup", win = storage.context === "win", tab = storage.context === "tab", page = storage.context === "page";
+          /*  */
+          chrome.contextMenus.create({"id": "tab", "type": "radio", "title": "Open in tab",  "contexts": ["browser_action"], "checked": tab, "onclick": contextmenu.click});
+          chrome.contextMenus.create({"id": "page", "type": "radio", "title": "Open in page",  "contexts": ["browser_action"], "checked": page, "onclick": contextmenu.click});
+          chrome.contextMenus.create({"id": "win", "type": "radio", "title": "Open in window",  "contexts": ["browser_action"], "checked": win, "onclick": contextmenu.click});
+          chrome.contextMenus.create({"id": "popup", "type": "radio", "title": "Open in popup",  "contexts": ["browser_action"], "checked": popup, "onclick": contextmenu.click});
+        });
+      }
+    }
+  };
+
+
+button.init(contextmenu.create.button);
+chrome.browserAction.onClicked.addListener(button.click);
--- foreground.js
+++ foreground.js
@@ -1,18 +1,44 @@
-console.log("Hola from the front");
+if(!config){
+    var config = {
+        "iframe": null,
+        "listener": function (request, sender, sendResponse) {    
+            if (request.action === "print") window.print();
+            if (request.action === "close") config.interface.hide();
+          },
+        "interface": {
+            "toggle": function () {
+                config.iframe = document.querySelector(".mark-on-page");
+                config.interface[config.iframe ? "hide" : "show"]();
+              },
+              "hide": function () {
+                config.iframe.remove();
+                // chrome.runtime.sendMessage({"action": "icon", "state": "OFF"});
+              },
+              "show": function () {
+                  let iframe = document.createElement("iframe");
+                  iframe.setAttribute("class", "mark-on-page")
+                  iframe.src = chrome.runtime.getURL("index.html");
+                  
+                  iframe.style.top = "0";
+                  iframe.style.left = "0";
+                  iframe.style.margin = "0";
+                  iframe.style.border = "0";
+                  iframe.style.padding = "0";
+                  iframe.style.width = "100%";
+                  iframe.style.height = "100%";
+                  iframe.style.outline = "none";
+                  iframe.style.position = "fixed";
+                  iframe.style.zIndex = "2147483647";
+                  iframe.style.background = "transparent";
+                  
+                  document.documentElement.appendChild(iframe);
 
-let iframe = document.createElement("iframe");
-iframe.src = chrome.runtime.getURL("index.html");
+              }
+        }
+    }
+    //TODO: does not do anything right now
+    chrome.runtime.onMessage.addListener(config.listener);
+}
 
-iframe.style.top = "0";
-iframe.style.left = "0";
-iframe.style.margin = "0";
-iframe.style.border = "0";
-iframe.style.padding = "0";
-iframe.style.width = "100%";
-iframe.style.height = "100%";
-iframe.style.outline = "none";
-iframe.style.position = "fixed";
-iframe.style.zIndex = "2147483647";
-iframe.style.background = "transparent";
+config.interface.toggle();
 
-document.documentElement.appendChild(iframe);
